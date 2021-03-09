0dcdea061e3a52bfe6b2e70e3a358d1829d2aca2
Rafi Shaik Mohammad <mohammadrafi44@gmail.com>
2021-03-09 09:54:54 +0300

    Api WebServices - Simple Get Request Commit

---
+++ SeleniumFramework/src/test/java/api_WebServices/Get_Request01.java
@@ -0,0 +1,27 @@
+// Source : https://www.youtube.com/watch?v=JJ7Tp7_fX4c
+ 
+package api_WebServices;
+
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+
+public class Get_Request01 {
+	
+	@Test
+	void test_01() {
+		
+		Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+		System.out.println(response.asString());
+		System.out.println(response.getBody().asString());
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getStatusLine());
+		System.out.println(response.getHeader("content-type"));
+		System.out.println(response.getTime());
+		
+		int statusCode = response.getStatusCode();
+		Assert.assertEquals(statusCode, 200);
+		
+	}
+}
